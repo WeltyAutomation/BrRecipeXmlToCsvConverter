770b975d175004411e9c16662e9a997c8f1a3e05
John Welty <john@weltyautomation.com>
2023-01-16 08:47:51 -0600

    Add single property sample data

--- XmlToCsvConverter.Tests/UnitTest1.cs
+++ XmlToCsvConverter.Tests/UnitTest1.cs
@@ -7,7 +7,21 @@ namespace XmlToCsvConverter.Tests
         [Fact]
         public void Test1()
         {
+            var data = GetSingleProperty();
 
+
+        }
+
+        string GetSingleProperty()
+        {
+            return @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<DATA>
+	<Element Name=""gDuctLineConfig"" Type=""PvParameter"">
+		<Group ID=""gDuctLineConfig"">
+			<Property ID=""CircuitDiagram"" DataType=""STRING"" Value=""CD-3542-00"" />
+		</Group>
+	</Element>
+</DATA>";
         }
     }
 }
